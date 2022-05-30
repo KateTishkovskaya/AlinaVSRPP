a4f1a3cd5867038bb7a37329f0a24dc84d6dc0fb
KateTishkovskaya <Kate.Tishkovskaya@gmail.com>
2022-05-30 21:06:34 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+using CRUD.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MySql.Data.MySqlClient;
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddSingleton();
+// Add services to the container.
+builder.Services.AddBrowserDetection();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SqlDbContext>(opt =>
+    opt.UseSqlServer(
+        "Server=(localdb)\\mssqllocaldb;Database=CRUD;Trusted_Connection=True;"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+app.Run();
+
+
+   
